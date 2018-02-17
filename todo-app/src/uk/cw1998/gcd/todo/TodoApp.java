a4cdc7d52f04f405249c2ae83fca0fb21132c6c5
Christopher Wilkinson - PC <cwilkinson000@gmail.com>
2018-02-17 16:04:28 +0000

    Initial commit Setup project

---
+++ todo-app/src/uk/cw1998/gcd/todo/TodoApp.java
@@ -0,0 +1,12 @@
+package uk.cw1998.gcd.todo;
+
+public class TodoApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+
+        System.exit(0);
+    }
+}
---
+++ todo-app/test/uk/cw1998/gcd/todo/TodoAppTest.java
@@ -0,0 +1,18 @@
+package uk.cw1998.gcd.todo;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TodoAppTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void aTest() {
+        assertTrue(true);
+    }
+}
